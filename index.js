b4ddf8af07851a007de879a9883a99f48fb1bad0
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-07 11:47:40 +0300

    start

---
+++ index.js
@@ -0,0 +1,38 @@
+// Test 1
+
+// Объяви две переменные, productName для названия товара и pricePerItem для хранения цены за штуку. При объявлении присвой переменным следующие характеристики товара:
+
+// название - строка "Droid"
+// цена за штуку - число 2000
+// Объявлена переменная productName
+// Значение переменной productName это строка "Droid"
+// Объявлена переменная pricePerItem
+// Значение переменной pricePerItem это число 2000
+
+// Change code below this line
+// const productName = "Droid";
+// const pricePerItem = 2000;
+// console.log(productName);
+// // 'Droid'
+
+// console.log(pricePerItem);
+// 2000
+
+// Test 2
+
+// Имя товара изменили на "Repair droid" и увеличили его цену на 1500 кредитов.
+//  Переопредели значения переменных pricePerItem и productName после их объявления.
+
+// Переменная pricePerItem объявлена с помощью let
+// При объявлении переменной pricePerItem присвоено значение - число 2000
+// Переменной pricePerItem присвоено новое значение, больше чем предыдущее на 1500
+// Переменная productName объявлена с помощью let
+// При объявлении переменной productName присвоено значение - строка "Droid"
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem += 1500;
+
+// Test 3
