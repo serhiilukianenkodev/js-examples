fb16072157bf7fc3d05a8988b1f06ba93b78e34c
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-19 13:47:55 +0300

    js hw 6 task 1 done

--- index.js
+++ index.js
@@ -35,4 +35,35 @@
 // productName = "Repair droid";
 // pricePerItem += 1500;
 
-// Test 3
+// **********************
+
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.
+
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const refs = {
+  categories: document.querySelector("#categories"),
+};
+
+console.log("Number of categories: ", refs.categories.children.length);
+
+[...refs.categories.children].forEach((item) => {
+  //   console.dir(item);
+  const firstEl = item.firstElementChild.textContent;
+  const secondEl = item.lastElementChild.children.length;
+  console.log("Category:", firstEl);
+  console.log("Elements:", secondEl);
+});
