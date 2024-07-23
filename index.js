9ac85996254e282571ec0373beb29720bb74628d
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-23 16:47:42 +0300

    some tasks

--- index.js
+++ index.js
@@ -256,38 +256,254 @@
 
 // ------------task 10 -----------
 
-const refs = {
-  controls: document.querySelector("#controls"),
-  boxes: document.querySelector("#boxes"),
-  input: controls.querySelector("input"),
-  createBtn: controls.querySelector("button[data-create]"),
-  destroyBtn: controls.querySelector("button[data-destroy]"),
+// const refs = {
+//   controls: document.querySelector("#controls"),
+//   boxes: document.querySelector("#boxes"),
+//   input: controls.querySelector("input"),
+//   createBtn: controls.querySelector("button[data-create]"),
+//   destroyBtn: controls.querySelector("button[data-destroy]"),
+// };
+
+// refs.createBtn.addEventListener("click", onCreateBtnClick);
+// refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+// function onCreateBtnClick() {
+//   createBoxes(refs.input.value);
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// function createBoxes(amount) {
+//   let markup = "";
+//   for (let i = 0; i < amount; i += 1) {
+//     markup += `<div style="width: ${30 + i * 10}px; height: ${
+//       30 + i * 10
+//     }px; background-color: ${getRandomHexColor()}"></div>`;
+//   }
+
+//   refs.boxes.innerHTML = markup;
+// }
+
+// function destroyBoxes() {
+//   refs.boxes.innerHTML = "";
+// }
+
+// ---------------------------------
+
+// // Потрібно створити функцію яка буде шукати нарциса. Головна умова нарциса знають всі, нарцис не знає нікого.
+// // Not found
+// const people1 = [
+//   {
+//     name: "Alex",
+//     know: ["Eva", "Jhon"],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Alex"],
+//   },
+//   {
+//     name: "Eva",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: ["Alex"],
+//   },
+// ];
+// //Not found
+// const people2 = [
+//   {
+//     name: "Alex",
+//     know: ["Eva", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: [],
+//   },
+//   {
+//     name: "Eva",
+//     know: [],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Alex"],
+//   },
+// ];
+
+// // jhon
+// const people3 = [
+//   {
+//     name: "Alex",
+//     know: ["Eva", "Jhon"],
+//   },
+//   {
+//     name: "Jhon",
+//     know: [],
+//   },
+//   {
+//     name: "Eva",
+//     know: ["Alex", "Jhon"],
+//   },
+//   {
+//     name: "Ivan",
+//     know: ["Jhon", "Alex"],
+//   },
+// ];
+
+// function findNarthiss(people) {
+//   let isKnown = "not found";
+//   for (let i = 0; i < people.length; i++) {
+//     const currentPerson = people[i];
+
+//     if (currentPerson.know.length) {
+//       continue;
+//     }
+
+//     const isFind = people.every(({ name, know }) => {
+//       return know.includes(currentPerson.name) || currentPerson.name === name;
+//     });
+
+//     if (isFind) {
+//       return currentPerson.name;
+//     }
+//   }
+//   return isKnown;
+// }
+
+// console.log(findNarthiss(people3));
+
+// --------------------------------------------------------------------------------------------------------//
+
+// // Представлений масив фільмів (films) кожен елемент масиву являється об'єктом в якого є назва фільму та масив з жанрами до яких він відноситься,
+// // але в масиві(genreIDs) тільки ID цих фільмів.
+// // Потрібно в кожен об'єкт додати ключ genreNames який буде масивом з назвами жанрів до яких цей фільм відноситься.
+// // Після чого відрендерити список фільмів як наведено в прикладі нижче(Expected result), якщо в списку жанрів більш ніж 2 фільми тоді рендеримо назву перших двох жанрів,
+// // а всі інші замінюємо на "Others".
+// // Співвідношення ID жанру та назви жанру знаходиться в об'єкті genresInfo.
+
+// // //Object example
+// // {
+// //     title: "Interceptor",
+// //     genreIDs: [
+// //         28,
+// //         53,
+// //         12,
+// //     ],
+// //     genreNames: [
+// //         "Action",
+// //         "Thriller",
+// //         "Adventure"
+// //     ]
+// // }
+
+// ////  Expected result
+// // Films list:
+// // 1. 'Interceptor: Action, Thriller, Others.'
+// // 2. 'Fantastic Beasts: The Secrets of Dumbledore: Fantasy, Adventure, Others.'
+// // 3. 'Last Seen Alive: Action, Thriller.'
+// // 4. 'Jurassic World Dominion: Science Fiction, Action, Others.'
+
+function renderFilms(films) {
+  {
+    console.log("Films list:");
+    return films.map(({ title, genreNames }, i) => {
+      const renderedGenres = genreNames.slice(0, 2);
+      if (genreNames.length > 2) renderedGenres.push("Others");
+
+      console.log(`${i + 1}. '${title}: ${renderedGenres.join(", ")}...'`);
+    });
+  }
+}
+
+const films = [
+  {
+    title: "Interceptor",
+    genreIDs: [28, 53, 12],
+  },
+  {
+    title: "Fantastic Beasts: The Secrets of Dumbledore",
+    genreIDs: [14, 12, 28],
+  },
+  {
+    title: "Last Seen Alive",
+    genreIDs: [28, 53],
+  },
+  {
+    title: "Jurassic World Dominion",
+    genreIDs: [878, 28, 12, 53],
+  },
+];
+
+const genresInfo = {
+  genres: [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
+  ],
 };
 
-refs.createBtn.addEventListener("click", onCreateBtnClick);
-refs.destroyBtn.addEventListener("click", destroyBoxes);
+const filmsWithGenres = addGenres(films);
+console.log("🚀 ~ filmsWithGenres:", filmsWithGenres);
+renderFilms(filmsWithGenres);
 
-function onCreateBtnClick() {
-  createBoxes(refs.input.value);
+function addGenres(films) {
+  return films.map((film) => {
+    return {
+      ...film,
+      genreNames: film.genreIDs.map((id) =>
+        findGenreById(id, genresInfo.genres)
+      ),
+    };
+  });
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+function findGenreById(id, genres) {
+  // console.log(id, genres);
+  return genres.find((genre) => genre.id === id).name;
 }
 
-function createBoxes(amount) {
-  let markup = "";
-  for (let i = 0; i < amount; i += 1) {
-    markup += `<div style="width: ${30 + i * 10}px; height: ${
-      30 + i * 10
-    }px; background-color: ${getRandomHexColor()}"></div>`;
-  }
+// //     .
 
-  refs.boxes.innerHTML = markup;
-}
+// --------------------------------------------------------------------------------------------------------//
 
-function destroyBoxes() {
-  refs.boxes.innerHTML = "";
-}
+// // Реалізуйте клас Student, який успадковуватиметься від класу User. Цей клас повинен мати такі властивості:
+// // name приватна властивість (ім'я, успадковується від User),
+// // surname приватна властивість (прізвище, успадковується від User),
+// // year (рік вступу до вузу).
+// // Клас повинен мати метод getFullName() (успадковується від User), за допомогою якого можна вивести одночасно ім'я та прізвище студента.
+// // Також клас повинен мати метод getCourse(), який виводитиме поточний курс студента (від 1 до 5, якщо значення перевищує 5  курс виводити що студент являєтсья випускником).
+// // Курс обчислюється так: потрібно від поточного року відняти рік вступу до вузу. Поточний рік отримаєте самостійно (читати документацію!!!).
+// // Приклад ініціалізації екземпляру класа:
+// // const student = new Student('Петрик', 'Пяточкин', 2019);
+
+// // student.getFullName(); //поверне 'Петрик Пяточкин'
+// // student.getCourse();   //поверне 3 (третій курс)
